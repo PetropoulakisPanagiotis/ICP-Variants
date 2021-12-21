77e8e2545a9311ed20ad1b764a1f32f17cd60873
Florian Donhauser <35968719+floriandonhauser@users.noreply.github.com>
2021-12-21 08:36:50 +0100

    Change or to ||

--- icp-variants/selection.h
+++ icp-variants/selection.h
@@ -27,7 +27,7 @@ class PointSelection {
             if (m_selectionMode == SELECT_ALL) 
                 return m_source.getPoints();
 
-            if (m_selectionMode == UNIFORM_SAMPLING or m_selectionMode == RANDOM_SAMPLING)
+            if (m_selectionMode == UNIFORM_SAMPLING || m_selectionMode == RANDOM_SAMPLING)
                 return m_points;
         }
 
@@ -38,7 +38,7 @@ class PointSelection {
             if (m_selectionMode == SELECT_ALL) 
                 return m_source.getNormals();;
 
-            if (m_selectionMode == UNIFORM_SAMPLING or m_selectionMode == RANDOM_SAMPLING)
+            if (m_selectionMode == UNIFORM_SAMPLING || m_selectionMode == RANDOM_SAMPLING)
                 return m_normals;
         }
 
