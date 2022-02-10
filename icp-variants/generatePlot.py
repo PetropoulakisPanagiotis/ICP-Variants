65f863a0ab039ab29dadcd6ce7de17a15d1e7e6d
Panagiotis Petropoulakis <petropoulakispanagiotis@gmail.com>
2022-02-10 14:16:02 +0200

    Add files via upload

--- icp-variants/generatePlot.py
+++ icp-variants/generatePlot.py
@@ -1,22 +1,61 @@
 import matplotlib.pyplot as plt
-import argparse
 
-parser = argparse.ArgumentParser(description='Create plot from logged errors')
-parser.add_argument('-p', '--path', type=str, required=True , help="File containing the logs")
-args = parser.parse_args()
+f = open("./colors/52/RMSE0.txt")
 
-path = args.path
+error = f.read()
 
-f = open(path)
+error = error.split("\n")
+error.pop()
+error1 = [float(error) for error in error]
 
-errors = f.read()
+f = open("./colors/11/RMSE0.txt")
 
-errors = errors.split("\n")
-errors.pop()
-errors = [float(error) for error in errors]
+error = f.read()
 
-plt.xlabel("step")
-plt.ylabel("Error")
-plt.plot(errors)
+error = error.split("\n")
+error.pop()
+error2 = [float(error) for error in error]
 
+f = open("./colors/53/RMSE0.txt")
+
+error = f.read()
+
+error = error.split("\n")
+error.pop()
+error3 = [float(error) for error in error]
+
+f = open("./colors/55/RMSE0.txt")
+
+error = f.read()
+
+error = error.split("\n")
+error.pop()
+error4 = [float(error) for error in error]
+
+f = open("./colors/29/RMSE0.txt")
+
+error = f.read()
+
+error = error.split("\n")
+error.pop()
+error5 = [float(error) for error in error]
+
+f = open("./colors/30/RMSE0.txt")
+
+error = f.read()
+
+error = error.split("\n")
+error.pop()
+error6 = [float(error) for error in error]
+
+plt.xlabel("Iteration")
+plt.ylabel("RMS alignment error")
+plt.plot(error1, label='Symmetric const weig. + Projective', marker="h")
+plt.plot(error2, label='Symmetric colors weig. + Projective', marker=".")
+plt.plot(error3, label='Symmetric constant weig. + Projective + Multi-Resolution', marker='x')
+plt.plot(error4, label='Symmetric colors weig. + Projective + Multi-Resolution', marker="_")
+#plt.plot(error2, label='Plane non-linear', marker="+")
+#plt.plot(error3, label='Symmetric non-linear', marker="_")
+
+plt.legend()
 plt.show()
